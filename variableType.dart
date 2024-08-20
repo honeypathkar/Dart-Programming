c11f65e206996f2e9a7189323c99ed9fd919fea7
honeypathkar <honeypatkar70@gmail.com>
2024-08-20 14:21:12 +0530

    Variable and Data Type in Dart

---
+++ variableType.dart
@@ -0,0 +1,14 @@
+//Declaration of variable
+void main() {
+  String name = 'Honey'; //String value
+  int age = 20; //Numeric value
+  double salary = 18.00; //Floating Value
+  BigInt value; //BigInt value
+  value = BigInt.parse('4445645152487465413541546');
+  num per = 87.40; //Number value any type of value can be stored eg. Int, float
+  print("String value = $name");
+  print("Integer value = $age");
+  print("Floating Point value = $salary");
+  print("BigInt value = $value");
+  print("Number value = $per");
+}
