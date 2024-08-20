a527858f89b7a3d17c1564b1cfd3c15b93bf5826
honeypathkar <honeypatkar70@gmail.com>
2024-08-20 13:48:36 +0530

    Taking input form user and defining variable

--- hello.dart
+++ hello.dart
@@ -1,3 +1,13 @@
+import 'dart:io';
+
 void main() {
+  //Printing value
+  //Using print() method
   print('Hello world !');
+  //Using stdout.write() method
+  stdout.write('Enter Your Name: ');
+
+  //Reading value form user and defining variable
+  var name = stdin.readLineSync();
+  print("Welcome, $name");
 }
